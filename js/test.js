ce073b4b8cf2858a2caa4532a269739850785e04
rnaxu <luv.s2.rnaxu@gmail.com>
2016-02-01 16:44:32 +0900

    add preloadjs

--- js/test.js
+++ js/test.js
@@ -1,8 +1,11 @@
 (function() {
 
-    function createAnimation() {
+    var stage;
+
+    // アニメーションを作る
+    function createAnimation(file) {
         var data = {
-            images: ['img/sample.png'], // スプライト画像のパス
+            images: [file], // スプライト画像のパス
             frames: {
                 width: 82, // 幅
                 height: 109, // 高さ
@@ -16,28 +19,54 @@
                 }
             }
         };
+
         var spriteSheet = new createjs.SpriteSheet(data);
         var animation = new createjs.Sprite(spriteSheet);
+
         return animation;
     }
 
-    function init() {
+    // ステージを更新
+    function handleTick(event) {
+        stage.update();
+    }
+
+    // アニメーションを表示する
+    function showAnimation(file) {
         var canvasElement = document.getElementById("demoCanvas");
         var stageWidth = canvasElement.width;
         var stageHeight = canvasElement.height;
-        var stage = new createjs.Stage(canvasElement);
 
-        var animation = createAnimation();
+        // ステージを作成
+        stage = new createjs.Stage(canvasElement);
+
+        var animation = createAnimation(file);
         animation.x = stageWidth / 2;
         animation.y = stageHeight / 2;
         animation.gotoAndPlay('walk');
+
+        // アニメーションをステージに加える
         stage.addChild(animation);
 
-        createjs.Ticker.addEventListener('tick', function() {
-            stage.update();
-        });
+        createjs.Ticker.addEventListener('tick', handleTick);
     }
 
-    init();
+    // 画像のパスを取得して、アニメーションを表示する関数に引数として渡す
+    function handleFileComplete(event) {
+        var image = event.item.src;
+        showAnimation(image);
+    }
+
+    // 画像を読み込む
+    function loadImage() {
+        var queue = new createjs.LoadQueue(true);
+
+        // 画像が一つ読み込まれたとき
+        queue.addEventListener('fileload', handleFileComplete);
+
+        queue.loadFile('img/sample.png');
+    }
+
+    loadImage();
 
 })();
